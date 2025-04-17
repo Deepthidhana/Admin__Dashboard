b267c235416809e06a63745e2c11195baaf58acb
DhanaVijaya Lokam <deepthidhana.gmail.com>
2025-04-17 18:18:54 +0530

    vitest initial configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setupTests.ts"],
+    // testMatch: ["./tests/**/*.test.tsx"],
+    coverage:{
+      thresholds:{
+        statements:90,
+        functions: 90,
+        lines:90,
+        branches:90
+      }
+    }
+  },
+});
